3a5ddc9c490678ab76d3049f3073dd7e617dff64
geninhocell <geninhocell@gmail.com>
2021-07-25 17:22:50 -0300

    end2

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
 
+import theme from './src/global/styles/theme';
 import {StorageProvider} from './src/hook/storage';
 import { AppRoutes } from './src/routes/app.routes';
 
@@ -17,10 +19,12 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <StorageProvider>
-        <AppRoutes />
-      </StorageProvider>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StorageProvider>
+          <AppRoutes />
+        </StorageProvider>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {    
+    background_button_primary: '#FECBB2',
+    primary: '#4E3975',
+    primary_light: '#9883BF',
+
+    shape: '#FFFFFF',
+
+    danger: '#D93025',
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+};
